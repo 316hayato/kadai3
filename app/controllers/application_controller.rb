9fbd86f4376a91b50f4302242c2171c44760067e
316hayato <131685231+316hayato@users.noreply.github.com>
2023-05-28 13:42:40 +0000

    STEP1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # サインイン後の遷移先を設定
   def after_sign_in_path_for(resource)
-    user_path
+    books_path
   end
   
   # サインアウト後の遷移先を設定
@@ -14,6 +15,6 @@ class ApplicationController < ActionController::Base
   protected
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,38 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
+  # 投稿データの保存
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+    redirect_to book_path
+    else
+      render :index
+    end
   end
   
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to books_path  # 投稿一覧画面へリダイレクト
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
